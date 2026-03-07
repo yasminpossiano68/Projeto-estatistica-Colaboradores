363acdfc4070430dd7ebab8a1c7cbeb304182d48
yasminpossiano68 <yasminpossiano68@gmail.com>
2026-03-06 21:20:33 -0300

    Revert "meu trabalho de mediana"

--- projeto-estatistica.py
+++ projeto-estatistica.py
@@ -1,26 +1,44 @@
+# Projeto Colaborativo - Ciencia de Dados
 dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
 
-def limpar_dados(lista):
-    dados_limpos = []
-    for item in lista:
-        if isinstance(item, (int, float)):
-            dados_limpos.append(item)
-    return dados_limpos
+def limpar_dados():
+    # Retorne uma lista apenas com int ou float
+    pass
 
+def calcular_media():
+ 
+    soma = 0
+    quantidade = 0
 
-def calcular_mediana(lista):
-    lista_ordenada = sorted(lista)
-    n = len(lista_ordenada)
+    for valor in dados:
+        soma += valor
+        quantidade += 1
 
-    if n % 2 == 0:
-        meio1 = lista_ordenada[n//2 - 1]
-        meio2 = lista_ordenada[n//2]
-        return (meio1 + meio2) / 2
-    else:
-        return lista_ordenada[n//2]
-
+    media = soma / quantidade
+    return media
 
 dados = limpar_dados(dados_sujos)
 
+media = calcular_media(dados)
+
 print(f"Dados processados: {dados}")
-print(f"Mediana: {calcular_mediana(dados)}")
+print(f"Média dos dados: {media}")
+
+def calcular_mediana():
+    pass
+
+def calcular_variancia():
+    pass
+
+def obter_extremos(dados):
+    menor = min(dados)
+    maior = max(dados)
+    return menor, maior
+
+dados = limpar_dados(dados_sujos)
+
+menor, maior = obter_extremos(dados)
+
+print(f" Dados processados : {dados}")
+print(f"Menor valor é: {menor}")
+print(f"Maior valor é: {maior}")
