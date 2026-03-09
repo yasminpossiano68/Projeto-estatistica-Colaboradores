daaf1636bc0ac69da85040d72b4e650bbfdd16ea
YASMIN FERREIRA POSSIANO <47806528806@info.local>
2026-03-09 18:39:55 -0300

    Projeto Estatistica

---
+++ projeto-estatistica.py
@@ -0,0 +1,22 @@
+# Projeto Colaborativo - Ciencia de Dados
+dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
+
+def limpar_dados():
+    # Retorne uma lista apenas com int ou float
+    pass
+
+def calcular_media():
+    pass
+
+def calcular_mediana():
+    pass
+
+def calcular_variancia():
+    pass
+
+def obter_extremos():
+    pass
+
+dados = limpar_dados(dados_sujos)
+
+print(f" Dados processados : {dados}")
