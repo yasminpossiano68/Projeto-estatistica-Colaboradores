aaad2a7073d8fa2e2ac8ccec242e27537decc344
YASMIN FERREIRA POSSIANO <47806528806@info.local>
2026-03-09 19:17:11 -0300

    Resultado final

--- projeto-estatistica.py
+++ projeto-estatistica.py
@@ -1,22 +1,58 @@
 # Projeto Colaborativo - Ciencia de Dados
-dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
+dados_sujos = [10, " erro ", 20, 30, 40, None, 50, 15, " falha ", 25]
 
-def limpar_dados():
+def limpar_dados(dados):
     # Retorne uma lista apenas com int ou float
-    pass
+    dados_limpos = []
+    
+    for item in dados:
+        if isinstance(item, (int, float)):
+            dados_limpos.append(item)
+    
+    return dados_limpos
 
-def calcular_media():
-    pass
+def calcular_media(dados):
+    soma = sum(dados)
+    quantidade = len(dados)
+    media = soma / quantidade
+    return media
 
-def calcular_mediana():
-    pass
+# igor mediana vini
+def calcular_mediana(dados):
+    dados_ordenados = sorted(dados)
+    n = len(dados_ordenados)
 
-def calcular_variancia():
-    pass
+    if n % 2 == 0:
+        meio1 = dados_ordenados[n//2 - 1]
+        meio2 = dados_ordenados[n//2]
+        return (meio1 + meio2) / 2
+    else:
+        return dados_ordenados[n//2]
 
-def obter_extremos():
-    pass
+def calcular_variancia(dados):
+    media = sum(dados) / len(dados)
+    soma_quadrados = sum((x - media) ** 2 for x in dados)
+    variancia = soma_quadrados / len(dados)
+    return variancia
+
+def obter_extremos(dados):
+    menor = min(dados)
+    maior = max(dados)
+    return menor, maior
+
 
 dados = limpar_dados(dados_sujos)
 
-print(f" Dados processados : {dados}")
+media = calcular_media(dados)
+variancia = calcular_variancia(dados)
+mediana = calcular_mediana(dados)
+
+print(f"Dados processados: {dados}")
+print(f"Média dos dados: {media}")
+print(f"Mediana dos dados: {mediana}")
+print(f"Variancia dos dados processados: {variancia}")
+
+menor, maior = obter_extremos(dados)
+
+print(f"Menor valor é: {menor}")
+print(f"Maior valor é: {maior}")
